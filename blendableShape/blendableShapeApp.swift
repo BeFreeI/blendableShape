cf8039e4793032813761cc85e9eb13d483e87b1a
TURKCELL/Pavel Nepogodin <pavel.nepogodin@consultant.turcell.com.tr>
2023-10-11 14:27:17 +0300

    Initial Commit

---
+++ blendableShape/blendableShapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  blendableShapeApp.swift
+//  blendableShape
+//
+//  Created by  Pavel Nepogodin on 11.10.23.
+//
+
+import SwiftUI
+
+@main
+struct blendableShapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
